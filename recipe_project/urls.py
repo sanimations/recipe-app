c2d1e01fc3fa368301195acbc16047242f8d0ee9
sanimations <smartucci157@gmail.com>
2024-04-23 18:22:37 -0400

    Added a login and logout view

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view, logout_view, logout_success
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('recipes.urls')),
     path('recipes/', include('recipes.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('logout_success/', logout_success, name='logout_success'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   #to display lists and details
 from .models import Recipe                #to access Recipe model
+from django.contrib.auth.mixins import LoginRequiredMixin   #to protect class-based view
 
-class RecipeListView(ListView):            #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):            #class-based view
     model = Recipe                         #specify model
     template_name = 'recipes/main.html'    #specify template 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
 
