ff4883c643cd11dc6497a5de212f42b9dfcd8347
sanimations <smartucci157@gmail.com>
2024-05-06 18:33:53 -0400

    Updated Recipe Search Form

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,8 +1,26 @@
+# from django import forms
+
+
+# class RecipesSearchForm(forms.Form):
+#     # Define form fields for searching recipes.
+#     # search_term
+#     recipe_name = forms.CharField(required=False, label="Search by name") # Search By Recipe Name Optional.
+#     # ingredient = forms.CharField(required=False, label="Search by ingredient") # Search By Ingredient Optional.
+#     # max_cooking_time = forms.IntegerField(required=False, label="Maximum cooking time (minutes)", min_value=0) # Max Cooking Time Optional.
+
+
 from django import forms
 
+
+CHART__CHOICES = (
+   
+    ('#1','Bar chart'),
+    ('#2', 'Pie chart'), 
+    ('#3', 'Line cart')
+    
+)
+
 class RecipesSearchForm(forms.Form):
-    # Define form fields for searching recipes.
-    # search_term
-    recipe_name = forms.CharField(required=False, label="Search by name") # Search By Recipe Name Optional.
-    ingredient = forms.CharField(required=False, label="Search by ingredient") # Search By Ingredient Optional.
-    max_cooking_time = forms.IntegerField(required=False, label="Maximum cooking time (minutes)", min_value=0) # Max Cooking Time Optional.
+    recipe_name= forms.CharField(max_length=100)
+    ingredients= forms.CharField(max_length=300)
+    chart_type= forms.ChoiceField(choices=CHART__CHOICES)
