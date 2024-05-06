bef2ca64c003ee840dc8877ff178487321db217d
sanimations <smartucci157@gmail.com>
2024-05-06 18:33:35 -0400

    Updated URLS with new graph url

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home
 from .views import RecipeListView, RecipeDetailView
 from .views import search_recipes
+from .views import graph_recipes
 
 app_name = 'recipes'
 
@@ -10,4 +11,5 @@ urlpatterns = [
    path('list/', RecipeListView.as_view(), name='list'),
    path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
    path('search_recipes', search_recipes, name='search-rec'),
+   path('graph_recipes', graph_recipes, name='graph-recipes'),
 ]
