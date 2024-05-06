7414bb40eace706c8b6c7cd976aeaf57968742ad
sanimations <smartucci157@gmail.com>
2024-05-06 18:34:11 -0400

    Updated utils.py getChart function

--- recipes/utils.py
+++ recipes/utils.py
@@ -1,66 +1,55 @@
 from io import BytesIO 
 import base64
 import matplotlib.pyplot as plt
+from recipes.models import Recipe
+from collections import Counter
+from django.shortcuts import get_object_or_404
 
 def get_recipename_from_id(val):
     recipename=Recipe.objects.get(id=val)
     return recipename
 
 def get_graph():
-   #create a BytesIO buffer for the image
-   buffer = BytesIO()         
+    #create a BytesIO buffer for the image
+    buffer = BytesIO()         
 
-   #create a plot with a bytesIO object as a file-like object. Set format to png
-   plt.savefig(buffer, format='png')
+    #create a plot with a bytesIO object as a file-like object. Set format to png
+    plt.savefig(buffer, format='png')
 
-   #set cursor to the beginning of the stream
-   buffer.seek(0)
+    #set cursor to the beginning of the stream
+    buffer.seek(0)
 
-   #retrieve the content of the file
-   image_png=buffer.getvalue()
+    #retrieve the content of the file
+    image_png=buffer.getvalue()
 
-   #encode the bytes-like object
-   graph=base64.b64encode(image_png)
+    #encode the bytes-like object
+    graph=base64.b64encode(image_png)
 
-   #decode to get the string as output
-   graph=graph.decode('utf-8')
+    #decode to get the string as output
+    graph=graph.decode('utf-8')
 
-   #free up the memory of buffer
-   buffer.close()
+    #free up the memory of buffer
+    buffer.close()
 
-   #return the image/graph
-   return graph
+    #return the image/graph
+    return graph
 
 #chart_type: user input o type of chart,
 #data: pandas dataframe
-def get_chart(chart_type, data, **kwargs):
-   #switch plot backend to AGG (Anti-Grain Geometry) - to write to file
-   #AGG is preferred solution to write PNG files
-   plt.switch_backend('AGG')
 
-   #specify figure size
-   fig=plt.figure(figsize=(6,3))
+def get_chart(chart_type, data):
 
-   #select chart_type based on user input from the form
-   if chart_type == '#1':
-       #plot bar chart between date on x-axis and quantity on y-axis
-       plt.bar(data['date_created'], data['quantity'])
+    #switch plot backend to AGG (Anti-Grain Geometry) - to write to file
+    #AGG is preferred solution to write PNG files
+    plt.switch_backend('AGG')
 
-   elif chart_type == '#2':
-       #generate pie chart based on the price.
-       #The book titles are sent from the view as labels
-       labels=kwargs.get('labels')
-       plt.pie(data['price'], labels=labels)
+    #specify figure size
+    fig=plt.figure(figsize=(6,3))
 
-   elif chart_type == '#3':
-       #plot line chart based on date on x-axis and price on y-axis
-       plt.plot(data['date_created'], data['price'])
-   else:
-       print ('unknown chart type')
+    #select chart_type based on user input from the form
+    if chart_type == '#1':
+        #plot bar chart between date on x-axis and quantity on y-axis
+        plt.bar(data['name'], data['ingredients'])
 
-   #specify layout details
-   plt.tight_layout()
-
-   #render the graph to file
-   chart =get_graph() 
-   return chart
+    else:
+        return None
