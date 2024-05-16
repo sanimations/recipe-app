e1fc0d1e0c14efe222dff770bfca649535dc967c
sanimations <smartucci157@gmail.com>
2024-05-16 18:42:31 -0400

    Updated settings and makemigrations

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -14,13 +14,12 @@ from pathlib import Path
 import os
 import dotenv
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
